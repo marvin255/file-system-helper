3031b09adbf95346cd50ba5c59d6ee8fd9bfec7f
marvin255 <postbox433@gmail.com>
2023-08-19 17:34:50 +0200

    Fix testRemoveIfExists

--- tests/FileSystemHelperImplTest.php
+++ tests/FileSystemHelperImplTest.php
@@ -44,7 +44,7 @@ class FileSystemHelperImplTest extends BaseCase
         $id = [self::class, 'provideRemove'];
 
         $dirWithContent = self::getPathToTestDir($id, 'dir_content');
-        self::getPathToTestFile($id, 'dir_content');
+        self::getPathToTestFile($id, 'dir_content', 'nested');
         self::getPathToTestFile($id, 'dir_content');
 
         return [
@@ -91,6 +91,83 @@ class FileSystemHelperImplTest extends BaseCase
                 '/test_file_not_exist.txt',
                 null,
                 FileSystemException::create("Can't find entity"),
+            ],
+            'remove with empty string' => [
+                '',
+                null,
+                FileSystemException::create("Can't create SplFileInfo"),
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideRemoveIfExists
+     */
+    public function testRemoveIfExists(string|\SplFileInfo $file, string $baseDir = null, \Exception $exception = null): void
+    {
+        $helper = new FileSystemHelperImpl($baseDir);
+
+        if ($exception) {
+            $this->expectExceptionObject($exception);
+        }
+
+        $helper->removeIfExists($file);
+
+        if (!$exception) {
+            $this->assertFileDoesNotExist($this->convertPathToString($file));
+        }
+    }
+
+    public static function provideRemoveIfExists(): array
+    {
+        $id = [self::class, 'provideRemoveIfExists'];
+
+        $dirWithContent = self::getPathToTestDir($id, 'dir_content');
+        self::getPathToTestFile($id, 'dir_content', 'nested');
+        self::getPathToTestFile($id, 'dir_content');
+
+        return [
+            'remove file out of restricted folder' => [
+                self::getPathToTestFile($id, 'dir_1'),
+                self::getPathToTestDir($id, 'dir_1', 'nested'),
+                FileSystemException::create('All paths must be within base directory'),
+            ],
+            'remove file within base folder' => [
+                self::getPathToTestFile($id, 'dir_2'),
+                self::getPathToTestDir($id, 'dir_2'),
+            ],
+            'remove file out of restricted folder by relative path' => [
+                self::getPathToTestDir($id, 'dir_3', 'nested') . '/../../',
+                self::getPathToTestDir($id, 'dir_3'),
+                FileSystemException::create('All paths must be within base directory'),
+            ],
+            'remove file with utf symbols in the name' => [
+                self::getPathToTestFile($id, 'dir_4', 'тест'),
+                self::getPathToTestDir($id, 'dir_4', 'тест'),
+            ],
+            'remove file without base folder set' => [
+                self::getPathToTestFile($id, 'dir_5'),
+            ],
+            'remove file with backslashes in name' => [
+                self::convertPathToString(
+                    self::getPathToTestFile($id, 'dir_6'),
+                    '\\'
+                ),
+                self::convertPathToString(
+                    self::getPathToTestDir($id, 'dir_6'),
+                    '\\'
+                ),
+            ],
+            'remove file object' => [
+                self::convertPathToSpl(
+                    self::getPathToTestFile($id, 'dir_7')
+                ),
+            ],
+            'remove dir' => [
+                $dirWithContent,
+            ],
+            'remove non existed entity' => [
+                '/test_file_not_exist.txt',
             ],
             'remove with empty string' => [
                 '',
